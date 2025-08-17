beda8ccf7fb3efd53229e2a9a058189b8529c75f
PrincessPi3 <human.bagel@gmail.com>
2025-08-16 22:26:37 -0600

    moar 7z shit and some refactoring

---
+++ cryptanalysis/clean_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Are you sure you want to delete all temporary files? (y/n) " confirm
+if [ "$confirm" != "y" ]; then
+  echo "Aborting cleanup."
+  exit 1
+fi
+
+echo "Cleaning all files"
+find . -type f -name "*.tmp" -exec rm -f {} \;
+find . -type f -name "*.bak.*" -exec rm -f {} \;
+find . -type f -name ".volume*" -exec rm -f {} \;
+echo "Done!"
