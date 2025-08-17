caaa3e1931740a6bf83b001f3e2753c8d05ada74
PrincessPi3 <human.bagel@gmail.com>
2025-08-16 23:22:30 -0600

    moar fixzes

--- cryptanalysis/checksums.sh
+++ cryptanalysis/checksums.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log_file="./cryptanalysis/sha512sum-recursive-$(date "+%Y%m%d-%H%M-%S").tmp"
 
-echo "Starting checksum calculation..."
+echo "Starting checksum calculation... $PWD"
 # sha512 of each file inside of to_encrypt, recursively
 find ./to_encrypt -type d -name ".git" -prune -o -type f -exec sha512sum {} \; | tee -a "$log_file" 2>/dev/null # silent on fail
 # do the same for the volumes and add to log
