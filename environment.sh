dbabcc637a22ebad7d573822d04493467e0c88eb
PrincessPi3 <human.bagel@gmail.com>
2025-09-26 16:26:36 -0600

    1758925597

--- environment.sh
+++ environment.sh
@@ -4,6 +4,9 @@
 # passphrase var export
 export passphrase="$(cat ./cryptanalysis/pass_used_in_testing.txt)"
 
+# passphrase var
+alias passphrase="$passphrase"
+
 # nuke and reset environment with 60 second sleep
 alias test_reset_sleep='echo -e "\nNuking and Resetting... wait 60 seconds...\n"; sleep 60; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing ./to_encrypt; tree ./to_encrypt; echo; find ./to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt'
 
