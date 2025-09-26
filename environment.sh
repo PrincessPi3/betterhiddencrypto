1a81672a5630d9699a4a91ecbaa1110edbd24c2c
PrincessPi3 <human.bagel@gmail.com>
2025-09-26 17:30:02 -0600

    1758929403

--- environment.sh
+++ environment.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # USAGE: source ./environment.sh
+to_encrypt_dir="/tmp/to_encrypt" # only in memory fs for security
+passphrase_file="./cryptanalysis/pass_used_in_testing.txt" # file containing passphrase for testing
+passphrase="$(cat $passphrase_file)" # passphrase for testing
 
 # passphrase var export
-export test_passphrase="$(cat ./cryptanalysis/pass_used_in_testing.txt)"
+export test_passphrase="$passphrase"
 
-# passphrase var
-alias test_passphrase="echo $test_passphrase"
+# passphrase alias
+alias test_passphrase="echo $passphrase"
 
 # nuke and reset environment with 60 second sleep
-alias test_reset_sleep='echo -e "\nNuking and Resetting... wait 60 seconds...\n"; sleep 60; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing /tmp/to_encrypt; tree /tmp/to_encrypt; echo; find /tmp/to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt; rm -rf /tmp/to_encrypt; source ./environment.sh'
+alias test_reset_sleep="echo -e '\nNuking and Resetting... wait 60 seconds...\n'; sleep 60; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing $to_encrypt_dir; tree $to_encrypt_dir; echo; find $to_encrypt_dir -type f -exec cat {} \; ; echo -e '\npassphrase:'; cat ./cryptanalysis/pass_used_in_testing.txt; source ./environment.sh'"
 
 # nuke and reset environment (no sleep)
-alias test_reset='echo -e "\nNuking and Resetting...\n"; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing /tmp/to_encrypt; tree /tmp/to_encrypt; echo; find /tmp/to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt; rm -rf /tmp/to_encrypt; source ./environment.sh'
+alias test_reset="echo -e '\nNuking and Resetting...\n'; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing $to_encrypt_dir; tree $to_encrypt_dir; echo; find $to_encrypt_dir -type f -exec cat {} \; ; echo -e '\npassphrase:'; cat ./cryptanalysis/pass_used_in_testing.txt; source ./environment.sh'"
 
 # sanity check to_encrypt dir
-alias test_sanity='echo -e "\nChecking /tmp/to_encrypt directory...\n"; tree /tmp/to_encrypt; echo -e "\npassphrase:"; find /tmp/to_encrypt -type f -exec cat {} \; ; cat ./cryptanalysis/pass_used_in_testing.txt;'
+alias test_sanity="echo -e '\nChecking $to_encrypt_dir directory...\n'; tree $to_encrypt_dir; echo -e '\npassphrase:'; find $to_encrypt_dir -type f -exec cat {} \; ; cat ./cryptanalysis/pass_used_in_testing.txt;"
 
 # update path cryptanalysis scripts
 export PATH="$PATH:$PWD/cryptanalysis"
