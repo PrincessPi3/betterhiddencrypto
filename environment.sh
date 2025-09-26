5f9baaa1d9c80756a0c769d2cbf3af9bbf94a917
PrincessPi3 <human.bagel@gmail.com>
2025-09-26 17:24:08 -0600

    1758929048

--- environment.sh
+++ environment.sh
@@ -8,13 +8,13 @@ export test_passphrase="$(cat ./cryptanalysis/pass_used_in_testing.txt)"
 alias test_passphrase="echo $test_passphrase"
 
 # nuke and reset environment with 60 second sleep
-alias test_reset_sleep='echo -e "\nNuking and Resetting... wait 60 seconds...\n"; sleep 60; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing ./to_encrypt; tree ./to_encrypt; echo; find ./to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt; rm -rf /tmp/to_encrypt; source ./environment.sh'
+alias test_reset_sleep='echo -e "\nNuking and Resetting... wait 60 seconds...\n"; sleep 60; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing /tmp/to_encrypt; tree /tmp/to_encrypt; echo; find /tmp/to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt; rm -rf /tmp/to_encrypt; source ./environment.sh'
 
 # nuke and reset environment (no sleep)
 alias test_reset='echo -e "\nNuking and Resetting...\n"; cd ~; rm -rf ~/betterhiddencrypto/; git clone https://github.com/PrincessPi3/betterhiddencrypto.git ~/betterhiddencrypto 2>/dev/null; cd ~/betterhiddencrypto; cp -r ./cryptanalysis/to_encrypt_testing /tmp/to_encrypt; tree /tmp/to_encrypt; echo; find /tmp/to_encrypt -type f -exec cat {} \; ; echo -e "\npassphrase:"; cat ./cryptanalysis/pass_used_in_testing.txt; rm -rf /tmp/to_encrypt; source ./environment.sh'
 
 # sanity check to_encrypt dir
-alias test_sanity='echo -e "\nChecking to_encrypt directory...\n"; tree /tmp/to_encrypt; echo -e "\npassphrase:"; find /tmp/to_encrypt -type f -exec cat {} \; ; cat ./cryptanalysis/pass_used_in_testing.txt;'
+alias test_sanity='echo -e "\nChecking /tmp/to_encrypt directory...\n"; tree /tmp/to_encrypt; echo -e "\npassphrase:"; find /tmp/to_encrypt -type f -exec cat {} \; ; cat ./cryptanalysis/pass_used_in_testing.txt;'
 
 # update path cryptanalysis scripts
 export PATH="$PATH:$PWD/cryptanalysis"
